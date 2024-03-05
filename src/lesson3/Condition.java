2633a49a87e31ada577346f90db459a061e17ccb
Alon <130448263+Aomine1234@users.noreply.github.com>
2024-03-05 22:45:58 +0700

    Добавил класс Условие и задал переменную boolean

---
+++ src/lesson3/Condition.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class Condition {
+    public static boolean isAlive;
+
+    public static void main(String[] args) {
+        isAlive = true;
+        System.out.println("Переменная isAlive принимает значение: " + isAlive );
+
+
+    }
+}
